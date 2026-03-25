cf5b04f6ee4e22fb9ca9d33c30388116c81ec69c
kp1153 <hamaramorcha1153@gmail.com>
2026-03-25 09:07:11 +0530

    force deploy

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
 import Hero from "@/components/Hero";
+import FixTeeth from "@/app/fix-teeth/page";
+import RCT from "@/app/rct/page";
+import Capping from "@/app/capping/page";
+import XRay from "@/app/xray/page";
+import Pyorrhea from "@/app/pyorrhea/page";
+import Braces from "@/app/braces/page";
+import Contact from "@/app/contact/page";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <FixTeeth />
+      <RCT />
+      <Capping />
+      <XRay />
+      <Pyorrhea />
+      <Braces />
+      <Contact />
     </main>
   );
 }
